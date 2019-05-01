3b8dd8d80008350a5c3a40fae3f216eaa6dc8cd1
MAC-SSD <muhammeteser29@gmail.com>
2019-05-01 12:12:22 +0300

    added if and ternary operators

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,13 @@
 'use strict';
 
+// if statement
+// ternaty operators
+// logical and opreators
+
 var app = {
   title: 'Indecision',
-  subtitle: ' Some cool subtitle'
+  subtitle: ' Some cool subtitle',
+  options: ['One', 'Two']
 };
 
 var template = React.createElement(
@@ -15,20 +20,32 @@ var template = React.createElement(
     app.title,
     ' '
   ),
-  React.createElement(
+  app.subtitle && React.createElement(
     'p',
     null,
     ' ',
     app.subtitle,
+    ' '
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' ',
+    app.options.length > 0 ? 'Here options' : 'No Options',
     ' '
   )
 );
 
 var user = {
   name: 'Andrew',
-  age: 26,
+  age: 19,
   locaition: 'New York'
 };
+
+function getLocation(locaition) {
+  if (locaition) return locaition;
+  return 'Unknown';
+}
 
 var templateTwo = React.createElement(
   'div',
@@ -37,10 +54,10 @@ var templateTwo = React.createElement(
     'h1',
     null,
     ' ',
-    user.name,
+    user.name ? user.name : 'Anonymous',
     ' '
   ),
-  React.createElement(
+  user.age >= 18 && React.createElement(
     'p',
     null,
     'Age: ',
@@ -50,7 +67,7 @@ var templateTwo = React.createElement(
     'p',
     null,
     'Location: ',
-    user.locaition
+    getLocation(user.locaition)
   )
 );
 
--- src/app.js
+++ src/app.js
@@ -1,26 +1,41 @@
+// if statement
+// ternaty operators
+// logical and opreators
+
 const app = {
   title: 'Indecision',
   subtitle: ' Some cool subtitle',
+  options: ['One', 'Two']
 }
 
 const template = (
   <div>
     <h1> {app.title} </h1>
-    <p> {app.subtitle} </p>
+    {app.subtitle && <p> {app.subtitle} </p>}
+    <p> {app.options.length > 0 ? 'Here options' : 'No Options'} </p>
   </div>
 );
 
 const user = {
   name: 'Andrew',
-  age: 26,
+  age: 19,
   locaition: 'New York'
+};
+
+function getLocation(locaition) {
+  if (locaition)
+    return locaition;
+  return 'Unknown';
 }
 
 const templateTwo = (
   <div>
-    <h1> {user.name} </h1>
-    <p>Age: {user.age}</p>
-    <p>Location: {user.locaition}</p>
+    <h1> {user.name ? user.name : 'Anonymous'} </h1>
+
+    {user.age >= 18 && <p>Age: {user.age}</p>}
+    <p>Location: {getLocation(user.locaition)}</p>
+
+    {}
   </div>
 );
 
