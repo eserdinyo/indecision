0dcf55c5bdffe9122a75b3f320a6ded2b504d6e8
MAC-SSD <muhammeteser29@gmail.com>
2019-05-01 21:13:23 +0300

    added classes 01

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,55 +1,139 @@
-"use strict";
+'use strict';
 
-var count = 0;
-var addOne = function addOne() {
-  count++;
-  renderCounterApp();
-};
-var minusOne = function minusOne() {
-  if (count > 0) count--;
-  renderCounterApp();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Some cool subtitle',
+  options: []
 };
 
-var reset = function reset() {
-  count = 0;
-  renderCounterApp();
+var Person = function () {
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+
+    _classCallCheck(this, Person);
+
+    this.name = name;
+    this.age = age;
+  }
+
+  _createClass(Person, [{
+    key: 'getGretting',
+    value: function getGretting() {
+      return 'Hi. I\'am ' + this.name + '!';
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this.name + ' is ' + this.age + ' years old.';
+    }
+  }]);
+
+  return Person;
+}();
+
+var me = new Person('Muhammet', 24);
+console.log(me.getDescription());
+console.log(me.getGretting());
+
+var other = new Person();
+console.log(other.getDescription());
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+
+var removeAll = function removeAll() {
+  app.options = [];
+  render();
+};
+
+var makeDecision = function makeDecision() {
+  var randNum = Math.floor(Math.random() * app.options.length);
+  var option = app.options[randNum];
+  alert(option);
+};
+
+var visibility = false;
+var toggleVisibility = function toggleVisibility() {
+  visibility = !visibility;
+  render();
 };
 
 var appRoot = document.getElementById('app');
 
-var renderCounterApp = function renderCounterApp() {
-
+var render = function render() {
   var template = React.createElement(
-    "div",
-    { className: "container" },
+    'div',
+    { className: 'container' },
     React.createElement(
-      "div",
-      { className: "wrapper" },
-      React.createElement(
-        "button",
-        { onClick: addOne, className: "btn" },
-        "+1"
-      ),
-      React.createElement(
-        "h3",
-        null,
-        "Count: ",
-        count,
-        " "
-      ),
-      React.createElement(
-        "button",
-        { onClick: minusOne, className: "btn" },
-        "-1"
-      )
+      'h1',
+      null,
+      ' ',
+      app.title,
+      ' '
     ),
     React.createElement(
-      "button",
-      { onClick: reset, className: "btn reset" },
-      "Reset"
+      'p',
+      null,
+      ' ',
+      app.subtitle,
+      ' '
+    ),
+    React.createElement(
+      'button',
+      { disabled: app.options.length == 0, className: 'btn', onClick: makeDecision },
+      'What Should I do?'
+    ),
+    React.createElement(
+      'button',
+      { onClick: removeAll, className: 'btn' },
+      'Remove All'
+    ),
+    React.createElement(
+      'button',
+      { onClick: toggleVisibility },
+      'Toggle'
+    ),
+    visibility && React.createElement(
+      'p',
+      null,
+      'Some Details'
+    ),
+    React.createElement(
+      'ol',
+      null,
+      app.options.map(function (option) {
+        return React.createElement(
+          'li',
+          { key: option },
+          ' ',
+          option
+        );
+      })
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        { className: 'btn' },
+        'Add Option'
+      )
     )
   );
   ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+render();
--- src/app.js
+++ src/app.js
@@ -1,37 +1,88 @@
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp();
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Some cool subtitle',
+  options: []
 };
-const minusOne = () => {
-  if(count > 0)
-    count--;
-  renderCounterApp();
+
+class Person {
+  constructor(name = 'Anonymous', age = 0) {
+    this.name = name;
+    this.age = age;
+  }
+  getGretting() {
+    return `Hi. I'am ${this.name}!`;
+  }
+  getDescription() {
+    return `${this.name} is ${this.age} years old.`
+  }
 }
 
-const reset = () => {
-  count = 0;
-  renderCounterApp();
+const me = new Person('Muhammet', 24);
+console.log(me.getDescription());
+console.log(me.getGretting());
+
+const other = new Person();
+console.log(other.getDescription());
+
+
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+
+const removeAll = () => {
+  app.options = [];
+  render();
+}
+
+const makeDecision = () => {
+  const randNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randNum];
+  alert(option);
+};
+
+let visibility = false;
+const toggleVisibility = () => {
+  visibility = !visibility;
+  render();
 }
 
 
 const appRoot = document.getElementById('app');
 
-
-const renderCounterApp = () => {
-
+const render = () => {
   const template = (
     <div className="container">
-      <div className="wrapper">
-        <button onClick={addOne} className="btn">+1</button>
-        <h3>Count: {count} </h3>
-        <button onClick={minusOne} className="btn">-1</button>
-      </div>
-      <button onClick={reset} className="btn reset">Reset</button>
+      <h1> {app.title} </h1>
+      <p> {app.subtitle} </p>
+      <button disabled={app.options.length == 0} className="btn" onClick={makeDecision} >What Should I do?</button>
+      <button onClick={removeAll} className="btn">Remove All</button>
+      <button onClick={toggleVisibility} >Toggle</button>
+      {
+        visibility && (
+          <p>Some Details</p>
+        )
+      }
+
+      <ol>
+        {
+          app.options.map(option => <li key={option} > {option}</li>)
+        }
+      </ol>
+
+      <form onSubmit={onFormSubmit} >
+        <input type="text" name="option" />
+        <button className="btn">Add Option</button>
+      </form>
     </div>
   );
   ReactDOM.render(template, appRoot);
 }
 
-renderCounterApp();
-
+render();
