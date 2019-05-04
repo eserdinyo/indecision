82075e95af47f1803bd045b6abbe6ede0c9292d8
nadal <muhammeteser29@gmail.com>
2019-05-05 00:17:55 +0300

    finished project

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,17 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
     }]
   },
-  devtool: 'cheap-module-source-map'
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+  }
 };
